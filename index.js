0ee3019da9c0345bc67a719dccc7b58d90a1c2de
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-13 13:48:33 +0200

    puhelinluettelo fullstack toivottavasti valmis

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
+app.use(express.static('dist'))
+
 const PORT = process.env.PORT || 3001;
 
 let persons = [
