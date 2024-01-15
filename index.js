8f1f5d1cccbdaa7aa788c7e81f0d6c4fbf7603d9
Iisakki Säkkinen <esakkinen@topcon.com>
2024-01-15 20:05:22 +0200

    puhelinluettelo 3.15-3.18 valmiita kaikki

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const { unknownEndpoint, errorHandler } = require('./middleware');
 const cors = require('cors');
+const app = express();
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+app.use(express.static('dist'))
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-app.use(cors());
-app.use(express.static('dist'))
 
 const Person = require('./models/person');
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(persons => {
         res.json(persons);
-    });
+    }).catch(error => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id).then(person => {
-        res.json(person);
-    }).catch(error => {
-        console.log(error);
-        res.status(404).end();
-    });
+        if (person) return res.json(person);
+        else return res.status(404).end();
+    }).catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Person.deleteOne({_id: req.params.id}).then(result => {
         res.status(204).end();
-    }).catch(error => {
-        res.status(204).send(err);
-    });
+    }).catch(error => next(error));
 });
 
-app.post('/api/persons', async (req, res) => {
+app.post('/api/persons', async (req, res, next) => {
     if (!req.body.name || !req.body.number) return res.status(400).json({ error: 'name or number missing' });
     
     const person = new Person({
@@ -53,20 +50,18 @@ app.post('/api/persons', async (req, res) => {
     
     person.save().then(savedPerson => {
         res.json(savedPerson);
-    }).catch(error => {
-        console.log(error);
-        res.status(400).end();
-    });
+    }).catch(error => next(error));
 });
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
     Person.findOneAndUpdate({_id: req.params.id}, {number: req.body.number}, {new: true})
         .then(updatedPerson => {
             res.json(updatedPerson);
-        }).catch(error => {
-            res.status(400).end();
-    });
+        }).catch(error => next(error));
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 app.get('/info', async (req, res) => {
     res.send(`<p>Phonebook has info for ${await Person.countDocuments({})} people</p><p>${new Date()}</p>`);
